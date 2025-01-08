3d86e2864b087c6d802590fa2f8ea90b5d7a3b00
pessi-v <pessimo@posteo.net>
2025-01-08 15:26:52 +0100

    initial commit

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@diffusionstudio/vits-web", to: "@diffusionstudio--vits-web.js" # @1.0.3
+pin "onnxruntime-web" # @1.18.0
+pin "piper-DeOu3H9E"
